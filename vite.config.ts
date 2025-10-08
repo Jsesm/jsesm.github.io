4762bfcdd69df2dd229c983f883dc4a3b824c254
Jaime Sesmero Verbo <Jaime.Sesmero@alu.uclm.es>
2025-10-08 13:13:16 +0200

    Prueba 8

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'jsesm.github.io' // <- reemplaza con el nombre de tu repo
+  base: '/' // <- reemplaza con el nombre de tu repo
 });
